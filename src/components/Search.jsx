4c820ce8a323f6dd1e658aa822bfca506dd012e8
Venkatesh Sedella <venkatesh.sedella@argano.com>
2024-08-26 00:09:40 +0530

    Added Search component with google programmable search engine feature with Pagination

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,31 +2,58 @@ import { useState } from "react";
 import SearchResult from "./SearchResult";
 
 const SEARCH_API =
-  "https://www.googleapis.com/customsearch/v1?key=AIzaSyAjAHi_qxAqW1KyJeNjcDwZ6jHWedoBSG4&cx=220350f05d56c4b1c&fields=items(title,link,htmlSnippet),queries(previousPage/startIndex,nextPage/startIndex)";
+  "https://www.googleapis.com/customsearch/v1?key=AIzaSyAjAHi_qxAqW1KyJeNjcDwZ6jHWedoBSG4&cx=220350f05d56c4b1c&fields=items(title,link,snippet),queries(previousPage/startIndex,nextPage/startIndex)";
 
 export default function Search() {
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState(null);
   const [searchString, setSearchString] = useState();
   const [searchInProgres, setSearchInProgres] = useState(false);
-  
+  const [pageNumber, setPageNumber] = useState(1);
+  const [error, setError] = useState(null);
   const handleChange = (e) => {
     setSearchString(e.target.value)
   };
 
-  const initiateSearch = async function () {
+  const initiateSearch = async function (searchType) {
     if(searchString && searchString.trim()){
         setSearchInProgres(true);
-        const searchResult = await fetch(`${SEARCH_API}&q=${searchString}&start=0`);
+        let startIndex = 0; 
+        let pageNumberToSet = 1;
+        if (searchType === 'previous'){
+            startIndex = searchResults ? searchResults?.queries?.previousPage[0]?.startIndex : 0;
+            pageNumberToSet = pageNumber - 1;
+        }else if(searchType === 'next'){
+            startIndex = searchResults ? searchResults?.queries?.nextPage[0]?.startIndex : 0;
+            pageNumberToSet = pageNumber + 1;
+        }
+        const searchResult = await fetch(`${SEARCH_API}&q=${searchString}&start=${startIndex}`);
         if (searchResult.ok) {
           const result = await searchResult.json();
           setSearchResults(result);
+          setPageNumber(pageNumberToSet);
+          setError(null);
+        }else{
+            const result = await searchResult.json();
+            setError(result.error.message);
+            setSearchResults(null);
         }
         setSearchInProgres(false);
+    }else{
+        alert('Enter a search string');
     }
   };
+  const disablePrevious = !searchResults?.queries?.previousPage;
+  const disableNext = !searchResults?.queries?.nextPage;
   
   return (
     <>
+        {searchInProgres &&
+            <div className="flex justify-center items-center h-screen fixed left-[50%]">
+                <div className="w-[60px] h-[60px] animate-ping border-2 border-indigo-500 rounded-full">
+                </div>
+            </div>
+        
+        }
       <div className="max-w-xl mx-auto mt-4 flex flex-row gap-1">
         <input
           type="text"
@@ -37,18 +64,17 @@ export default function Search() {
         ></input>
 
         <button
-          onClick={initiateSearch}
+          onClick={() => initiateSearch('base')}
           type="button"
           className="w-1/5 bg-indigo-500 hover:bg-indigo-600 text-xl text-white font-bold py-2 px-4 focus:outline-none focus:border-indigo-700 rounded-full"
         >
           Submit
         </button>
       </div>
-      <p> Search results are here : {searchResults.length}</p>
-      <div className="mt-2">
-        {searchInProgres && <p> Search In Progress </p>}
-        {searchResults?.items.length && (
-            <div className="ml-10 mr-10">
+      {error && <p className="font-bold text-red-400 mt-2"> { error }</p>}
+      { searchResults?.items?.length && <div className="mt-2 ">
+        <div className="max-h-[600px] overflow-x-hidden overflow-y-scroll">
+        <div className="ml-10 mr-10">
 
           <ul>
             {searchResults.items.map((result, index) => (
@@ -56,8 +82,22 @@ export default function Search() {
             ))}
           </ul>
             </div>
-        )}
-      </div>
+        </div>
+        <div className="flex flex-row gap-10 justify-center items-center">
+            <button 
+                type="button" 
+                className={`text-indigo-500 font-bold ${disablePrevious ? 'cursor-not-allowed' : 'cursor-pointer'}`} 
+                disabled={disablePrevious}
+                onClick={() => initiateSearch('previous')}> Previous </button>
+            <button type="button" className="text-indigo-500 font-bold cursor-default"> Page {pageNumber} </button>
+            <button 
+                type="button" 
+                className={`text-indigo-500 font-bold ${disableNext ? 'cursor-not-allowed' : 'cursor-pointer'}`} 
+                disabled={disableNext}
+                onClick={() => initiateSearch('next')}> Next </button>
+        </div>
+      </div>}
+
     </>
   );
 }
--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,11 +1,11 @@
-export default function SearchResult({title, link, htmlSnippet}){
+export default function SearchResult({title, link, snippet}){
     return <div className="h-20 border border-indigo-100 flex flex-row gap-2 mb-4 p-2">
-        <div>
-            <span> {title} </span>
-            <span> {htmlSnippet} </span>
+        <div className="flex flex-col w-4/5">
+            <span className="font-bold"> {title} </span>
+            <span> {snippet} </span>
         </div>
-        <div>
-            <a href={link} target="_blank"> Open </a>
+        <div className="w-1/5 flex justify-center items-center">
+            <a href={link} target="_blank" className="bg-indigo-500 hover:bg-indigo-600 text-xl text-white font-bold py-2 px-4 focus:outline-none focus:border-indigo-700 rounded-full"> Open </a>
         </div>
     </div>
 }
